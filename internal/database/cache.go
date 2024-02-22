358c62387c5f4778446616c51ffa13123438c103
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-02-21 22:27:26 -0800

    move models to database package
    models package will now hold the API facing structs

---
+++ internal/database/cache.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/allegro/bigcache/v3"
+	"github.com/rs/zerolog/log"
+)
+
+var Cache *bigcache.BigCache
+var SRPServerKey = "srp-server-struct:%d"
+
+func init() {
+	var err error
+	Cache, err = bigcache.New(
+		context.Background(),
+		bigcache.Config{
+			Shards:      1024,
+			LifeWindow:  5 * time.Minute,
+			CleanWindow: 1 * time.Minute,
+		},
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to initalize in-memory cache")
+	}
+}
--- internal/database/redis.go
+++ internal/database/redis.go
@@ -4,4 +4,4 @@ import "github.com/redis/go-redis/v9"
 
 var Redis *redis.Client
 
-var RedisSessionKeyPrefix string = "session_key"
+var RedisSessionKeyPrefix string = "session_key:%d"
