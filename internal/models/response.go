7985f740a9f3f90d496b74669ed9042b09c4f44a
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-02-21 23:59:21 -0800

    add error and success response structs

--- internal/models/response.go
+++ internal/models/response.go
@@ -2,6 +2,11 @@ package models
 
 // response.go contains struct types for the responses returned by this API
 
+type ErrorResponse struct {
+	Status int    `json:"status"`
+	Error  string `json:"error"`
+}
+
 type IdentityResponse struct {
 	Salt            string `json:"salt"`
 	EphemeralPublic string `json:"B"`
@@ -9,4 +14,9 @@ type IdentityResponse struct {
 
 type VerificationResponse struct {
 	ServerProof string `json:"proof"`
+}
+
+type SuccessResponse struct {
+	Status int    `json:"status"`
+	Data   string `json:"data"`
 }
