430ae668947e20764672416a01a4aaec1e7dde8c
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-04-09 21:43:10 -0700

    make our own funtions to serve pages
    so i don't have to see "status": 0 in the logs for these pages :--)

--- internal/html/render.go
+++ internal/html/render.go
@@ -3,16 +3,26 @@ package html
 import (
 	"net/http"
 
-	"github.com/a-h/templ"
 	"github.com/go-chi/render"
 	"github.com/heyztb/lists-backend/internal/html/templates/pages"
 )
 
-// var ServeHomePage = templ.Handler(pages.Home("Lists")).ServeHTTP
-var ServeRegisterPage = templ.Handler(pages.Register()).ServeHTTP
-var ServeLoginPage = templ.Handler(pages.Login()).ServeHTTP
-
 func ServeHomePage(w http.ResponseWriter, r *http.Request) {
 	render.Status(r, http.StatusOK)
 	pages.Home("Lists").Render(r.Context(), w)
+}
+
+func ServeRegisterPage(w http.ResponseWriter, r *http.Request) {
+	render.Status(r, http.StatusOK)
+	pages.Register().Render(r.Context(), w)
+}
+
+func ServeLoginPage(w http.ResponseWriter, r *http.Request) {
+	render.Status(r, http.StatusOK)
+	pages.Login().Render(r.Context(), w)
+}
+
+func ServeAboutPage(w http.ResponseWriter, r *http.Request) {
+	render.Status(r, http.StatusOK)
+	pages.About().Render(r.Context(), w)
 }
