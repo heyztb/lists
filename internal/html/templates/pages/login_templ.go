faae427348fab6f298e29204357692000b99596d
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-04-11 08:57:09 -0700

    mage templ

--- internal/html/templates/pages/login_templ.go
+++ internal/html/templates/pages/login_templ.go
@@ -13,6 +13,25 @@ import "bytes"
 import "github.com/heyztb/lists-backend/internal/html/templates/shared"
 import "github.com/heyztb/lists-backend/internal/html/templates/partials"
 
+func submitLogin() templ.ComponentScript {
+	return templ.ComponentScript{
+		Name: `__templ_submitLogin_1889`,
+		Function: `function __templ_submitLogin_1889(){event.preventDefault()
+  const form = new FormData(event.target)
+  const data = Object.fromEntries(form)
+  try {
+    srp.loginUser(data['email'], data['password']).then(key => {
+      console.log(key)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}`,
+		Call:       templ.SafeScript(`__templ_submitLogin_1889`),
+		CallInline: templ.SafeScriptInline(`__templ_submitLogin_1889`),
+	}
+}
+
 func Login() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
@@ -32,7 +51,7 @@ func Login() templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!--\n    // v0 by Vercel.\n    // https://v0.dev/t/Ui6lPpyAQKV\n    --> <script type=\"module\" src=\"/assets/srp.js\">\n\t\t  const onSubmit = async (event) => {\n\t\t    event.preventDefault()\n\t\t    const form = new FormData(event.target)\n\t\t    const data = Object.fromEntries(form)\n\t\t    console.log(data)\n\t\t  }\n\t\t</script> <div class=\"w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]\">")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!--\n    // v0 by Vercel.\n    // https://v0.dev/t/Ui6lPpyAQKV\n    --> <script type=\"module\" src=\"/assets/srp.min.js\"></script> <div class=\"w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -40,7 +59,24 @@ func Login() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"hidden bg-muted lg:block\"><img src=\"/placeholder.svg\" alt=\"Image\" width=\"1920\" height=\"1080\" class=\"h-full w-full object-cover dark:brightness-[0.2] dark:grayscale\" style=\"aspect-ratio: 1920 / 1080; object-fit: cover;\"></div><div class=\"flex items-center justify-center py-12\"><div class=\"mx-auto grid w-[350px] gap-6\"><div class=\"grid gap-2 text-center\"><h1 class=\"text-3xl font-bold\">Login</h1><p class=\"text-balance text-muted-foreground\">Enter your email below to login to your account</p></div><form class=\"grid gap-4\" onsubmit=\"onSubmit(this)\" method=\"POST\"><div class=\"grid gap-2\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"email\">Email</label> <input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"email\" id=\"email\" placeholder=\"m@example.com\" required=\"\" type=\"email\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"password\">Password</label> <a class=\"ml-auto inline-block text-sm underline\" href=\"#\">Forgot your password?</a></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"password\" id=\"password\" required=\"\" type=\"password\"></div><div class=\"flex flex-col gap-2\"><button class=\"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex-1\" type=\"submit\">Login</button></div></form></div></div></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"hidden bg-muted lg:block\"><img src=\"/placeholder.svg\" alt=\"Image\" width=\"1920\" height=\"1080\" class=\"h-full w-full object-cover dark:brightness-[0.2] dark:grayscale\" style=\"aspect-ratio: 1920 / 1080; object-fit: cover;\"></div><div class=\"flex items-center justify-center py-12\"><div class=\"mx-auto grid w-[350px] gap-6\"><div class=\"grid gap-2 text-center\"><h1 class=\"text-3xl font-bold\">Login</h1><p class=\"text-balance text-muted-foreground\">Enter your email below to login to your account</p></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templ.RenderScriptItems(ctx, templ_7745c5c3_Buffer, submitLogin())
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<form class=\"grid gap-4\" onSubmit=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var3 templ.ComponentScript = submitLogin()
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ_7745c5c3_Var3.Call)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" method=\"POST\"><div class=\"grid gap-2\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"email\">Email</label> <input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"email\" id=\"email\" placeholder=\"m@example.com\" required=\"\" type=\"email\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"password\">Password</label> <a class=\"ml-auto inline-block text-sm underline\" href=\"#\" tabindex=\"3\">Forgot your password?</a></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"password\" id=\"password\" required=\"\" type=\"password\"></div><div class=\"flex flex-col gap-2\"><button class=\"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex-1\" type=\"submit\">Login</button></div></form></div></div></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
--- internal/html/templates/pages/register_templ.go
+++ internal/html/templates/pages/register_templ.go
@@ -13,6 +13,26 @@ import "bytes"
 import "github.com/heyztb/lists-backend/internal/html/templates/shared"
 import "github.com/heyztb/lists-backend/internal/html/templates/partials"
 
+func submitRegistration() templ.ComponentScript {
+	return templ.ComponentScript{
+		Name: `__templ_submitRegistration_1d2f`,
+		Function: `function __templ_submitRegistration_1d2f(){event.preventDefault()
+  const form = new FormData(event.target)
+  const data = Object.fromEntries(form)
+  console.log(data)
+  try {
+    registerUser(data['email'], data['password']).then(success => {
+      console.log(success)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}`,
+		Call:       templ.SafeScript(`__templ_submitRegistration_1d2f`),
+		CallInline: templ.SafeScriptInline(`__templ_submitRegistration_1d2f`),
+	}
+}
+
 func Register() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
@@ -32,7 +52,7 @@ func Register() templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!--\n    // v0 by Vercel.\n    // https://v0.dev/t/WeXFtpsSsmG\n    --> <div class=\"w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]\">")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!--\n    // v0 by Vercel.\n    // https://v0.dev/t/WeXFtpsSsmG\n    --> <script type=\"module\" src=\"/assets/srp.min.js\">\n\t\t  function onSubmit(event) {\n\t\t    event.preventDefault()\n        const form = new FormData(event.target)\n        const data = Object.fromEntries(form)\n        console.log(data)\n        try {\n          srp.registerUser(data['email'], data['password']).then(success => {\n            console.log(success)\n          })\n        } catch (e) {\n          console.log(e)\n        }\n\t\t  }\n\t\t</script> <div class=\"w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -40,7 +60,7 @@ func Register() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"hidden bg-muted lg:block\"><h1 class=\"text-4xl font-bold lg:text-5xl\">Lists has been created with your privacy and data security in mind.</h1></div><div class=\"flex items-center justify-center py-12\"><div class=\"mx-auto grid w-[350px] gap-6\"><div class=\"grid gap-2 text-center\"><h1 class=\"text-3xl font-bold\">Create an account</h1><p class=\"text-balance text-muted-foreground\">Get started with Lists today</p></div><form class=\"grid gap-4\" onsubmit=\"onSubmit(this)\" method=\"POST\"><div class=\"grid gap-2\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"email\">Email</label> <input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"email\" id=\"email\" placeholder=\"m@example.com\" required type=\"email\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"password\">Password</label></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"password\" id=\"password\" required type=\"password\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"confirm-password\">Confirm password</label></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"confirm-password\" id=\"confirm-password\" required type=\"password\"></div><div class=\"flex flex-col gap-2\"><button class=\"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex-1\" type=\"submit\">Create account</button></div></form></div></div></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"hidden bg-muted lg:block\"><h1 class=\"text-4xl font-bold lg:text-5xl\">Lists has been created with your privacy and data security in mind.</h1></div><div class=\"flex items-center justify-center py-12\"><div class=\"mx-auto grid w-[350px] gap-6\"><div class=\"grid gap-2 text-center\"><h1 class=\"text-3xl font-bold\">Create an account</h1><p class=\"text-balance text-muted-foreground\">Get started with Lists today</p></div><form class=\"grid gap-4\" onSubmit=\"onSubmit()\" method=\"POST\"><div class=\"grid gap-2\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"email\">Email</label> <input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"email\" id=\"email\" placeholder=\"m@example.com\" required type=\"email\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"password\">Password</label></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"password\" id=\"password\" required type=\"password\"></div><div class=\"grid gap-2\"><div class=\"flex items-center\"><label class=\"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70\" for=\"confirm-password\">Confirm password</label></div><input class=\"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50\" name=\"confirm-password\" id=\"confirm-password\" required type=\"password\"></div><div class=\"flex flex-col gap-2\"><button class=\"inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 flex-1\" type=\"submit\">Create account</button></div></form></div></div></div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
