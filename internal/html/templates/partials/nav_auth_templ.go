0855fe6ac101bd967cb13b2d6236eac0de9ecbd6
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-04-22 23:26:30 -0700

    nav partials

--- internal/html/templates/partials/nav_auth_templ.go
+++ internal/html/templates/partials/nav_auth_templ.go
@@ -23,7 +23,7 @@ func NavAuth(login bool) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header class=\"px-4 lg:px-6 h-14 flex items-center fixed top-0 left-0 right-0\"><a class=\"flex items-center justify-center\" href=\"/\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"h-6 w-6\"><path d=\"m8 3 4 8 5-5 5 15H2L8 3z\"></path></svg> <span class=\"sr-only\">Lists</span></a><nav class=\"ml-auto flex gap-4 sm:gap-6\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<header class=\"px-4 lg:px-6 h-14 flex items-center fixed top-0 left-0 right-0 z-50\"><a class=\"flex items-center justify-center\" href=\"/\"><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"h-6 w-6\"><path d=\"m8 3 4 8 5-5 5 15H2L8 3z\"></path></svg> <span class=\"sr-only\">Lists</span></a><nav class=\"ml-auto flex gap-4 sm:gap-6\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
