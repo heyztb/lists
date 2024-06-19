d03b53305cda7fcc4f2621595c410d399c77b039
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-06-19 14:10:58 -0700

    pass the whole user to the sidebar profile component

--- internal/html/templates/components/nav/sidebar_templ.go
+++ internal/html/templates/components/nav/sidebar_templ.go
@@ -39,7 +39,7 @@ func Sidebar(user database.User) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = dropdowns.SidebarProfile(user.Name.String).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = dropdowns.SidebarProfile(user).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
