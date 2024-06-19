0483dca2e71bb67194df5a8b4a5ae3ed4a7e6f86
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-06-18 22:53:32 -0700

    update max request size

--- internal/middleware/size.go
+++ internal/middleware/size.go
@@ -2,12 +2,12 @@ package middleware
 
 import "net/http"
 
-// Size middleware will limit all incoming request body's to 1MiB to prevent accidental or malicious requests from taking up resources
+// Size middleware will limit all incoming request body's to 32MB to prevent accidental or malicious requests from taking up resources
 type Size struct {
 	Mux http.Handler
 }
 
 func (s *Size) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.Body = http.MaxBytesReader(w, r.Body, 1<<20)
+	r.Body = http.MaxBytesReader(w, r.Body, 32<<20)
 	s.Mux.ServeHTTP(w, r)
 }
