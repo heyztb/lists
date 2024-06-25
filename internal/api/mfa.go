60e1ee391a9afe5cc59d9fd909f6ac6464376ef1
Zach Blake <18064509+heyztb@users.noreply.github.com>
2024-06-24 21:47:37 -0700

    add totp verification handler

--- internal/api/mfa.go
+++ internal/api/mfa.go
@@ -1,17 +1,71 @@
 package api
 
 import (
+	"encoding/base32"
 	"net/http"
 
 	cmw "github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/heyztb/lists/internal/crypto"
 	"github.com/heyztb/lists/internal/database"
 	"github.com/heyztb/lists/internal/log"
 	"github.com/heyztb/lists/internal/middleware"
+	"github.com/pquerna/otp/totp"
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
+
+func VerifyTOTPCodeHandler(w http.ResponseWriter, r *http.Request) {
+	requestID, _ := r.Context().Value(cmw.RequestIDKey).(string)
+	log := log.Logger.With().Str("request_id", requestID).Logger()
+	userID, _, _, err := middleware.ReadContext(r)
+	if err != nil {
+		log.Err(err).Msg("error reading session context")
+		render.Status(r, http.StatusInternalServerError)
+		w.Header().Add("HX-Redirect", "/500")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	user, err := database.Users(
+		database.UserWhere.ID.EQ(userID),
+		qm.Load(database.UserRels.Setting),
+	).One(r.Context(), database.DB)
+	if err != nil {
+		log.Err(err).Msg("error finding user from database")
+		render.Status(r, http.StatusInternalServerError)
+		w.Header().Add("HX-Redirect", "/500")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if !user.R.Setting.MfaEnabled {
+		render.Status(r, http.StatusBadRequest)
+		w.Header().Add("HX-Redirect", "/login")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	b32NoPadding := base32.StdEncoding.WithPadding(base32.NoPadding)
+	totpSecretBytes, err := crypto.AESDecrypt(crypto.ServerEncryptionKey, user.MfaSecret.Bytes)
+	if err != nil {
+		log.Err(err).Msgf("error decrypting totp secret for client %s", userID)
+		render.Status(r, http.StatusInternalServerError)
+		w.Header().Add("HX-Redirect", "/500")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	totpSecret := b32NoPadding.EncodeToString(totpSecretBytes)
+	code := r.FormValue("code")
+	if !totp.Validate(code, totpSecret) {
+		log.Error().Msgf("bad totp code from client %s", userID)
+		render.Status(r, http.StatusBadRequest)
+		w.Header().Add("HX-Redirect", "/login")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	render.Status(r, http.StatusOK)
+	w.Header().Add("HX-Redirect", "/app")
+	w.WriteHeader(http.StatusOK)
+}
 
 func Disable2FAHandler(w http.ResponseWriter, r *http.Request) {
 	requestID, _ := r.Context().Value(cmw.RequestIDKey).(string)
@@ -41,7 +95,7 @@ func Disable2FAHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	user.MfaSecret = null.StringFromPtr(nil)
+	user.MfaSecret = null.BytesFrom(nil)
 	user.MfaRecoveryCodes = []string{}
 	user.R.Setting.MfaEnabled = false
 	_, err = user.Update(r.Context(), database.DB, boil.Whitelist(
@@ -67,5 +121,4 @@ func Disable2FAHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Add("HX-Redirect", "/app/settings")
 	w.WriteHeader(http.StatusNoContent)
-	return
 }
